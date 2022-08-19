6e4d6ff10a227b436617ce3ebd319daffe25504c
amitguptaREAN <amit.gupta2@hitachivantara.com>
2022-08-19 17:08:47 -0500

    fix training and preprocesing code

--- pipelines/bankcd/preprocess.py
+++ pipelines/bankcd/preprocess.py
@@ -3,10 +3,13 @@ import numpy as np
 import pandas as pd
 from time import gmtime, strftime
 import logging
-logging.info('bankcd:preprocess.py starts')
+logger = logging.getLogger('log')
+logging.basicConfig(level=logging.INFO) # Set logging level and STDOUT handler
+
+logger.info('bankcd:preprocess.py starts')
 
 # prefix = 'sagemaker/DEMO-xgboost-dm'
-prefix = "/opt/ml/processing"
+prefix = "xgb"
 my_region = 'us-east-1' #boto3.session.Session().region_name # set the region of the instance
 bucket_name = 'tcb-bankcd' # <--- CHANGE THIS VARIABLE TO A UNIQUE NAME FOR YOUR BUCKET
 
@@ -23,27 +26,27 @@ s3 = boto3.resource('s3')
 
 # Download the bank dataset as a csv file.
 try:
-    urllib.request.urlretrieve ("https://d1.awsstatic.com/tmt/build-train-deploy-machine-learning-model-sagemaker/bank_clean.27f01fbbdf43271788427f3682996ae29ceca05d.csv", "bank_clean.csv")
-    logging.info('Success: Data loaded into dataframe.')
+  urllib.request.urlretrieve ("https://d1.awsstatic.com/tmt/build-train-deploy-machine-learning-model-sagemaker/bank_clean.27f01fbbdf43271788427f3682996ae29ceca05d.csv", "bank_clean.csv")
+  logger.info('Success: Data loaded into dataframe.')
 except Exception as e:
-  logging.error('Data load error: ',e)
+  logger.error('Data load error: ',e)
 
 # Read downloaded csv as a dataframe.
 try:
     model_data = pd.read_csv('./bank_clean.csv',index_col=0)
-    logging.info('Success: Data loaded into dataframe.')
+    logger.info('Success: Data loaded into dataframe.')
 except Exception as e:
-    logging.error('Data load error ', e)
+    logger.error('Data load error ', e)
 
 # Create train and test data sets and upload data sets to AWS directory
 train_data, test_data = np.split(model_data.sample(frac=1, random_state=1729), [int(0.7 * len(model_data))])
-print(train_data.shape, test_data.shape)
+logger.info(train_data.shape, test_data.shape)
 pd.concat([train_data['y_yes'], train_data.drop(['y_no', 'y_yes'], axis=1)], axis=1).to_csv(f"{prefix}/train/train.csv", index=False, header=False)
 pd.concat([test_data['y_yes'], test_data.drop(['y_no', 'y_yes'], axis=1)], axis=1).to_csv(f"{prefix}/test/test.csv", index=False, header=False)
 
-logging.info('Success: pre-processed bank_clean.csv.')
+logger.info('Success: pre-processed bank_clean.csv.')
 
 # Upload training data into S3 bucket
-boto3.Session().resource('s3').Bucket(bucket_name).Object(os.path.join(prefix, 'train/train.csv')).upload_file('/opt/ml/processing/train/train.csv')
-boto3.Session().resource('s3').Bucket(bucket_name).Object(os.path.join(prefix, 'train/test.csv')).upload_file('/opt/ml/processing/test/test.csv')
-logging.info('bankcd:preprocess.py ends')
+boto3.Session().resource('s3').Bucket(bucket_name).Object(os.path.join(prefix, 'train/train.csv')).upload_file(f"{prefix}/train/train.csv")
+boto3.Session().resource('s3').Bucket(bucket_name).Object(os.path.join(prefix, 'test/test.csv')).upload_file(f"{prefix}/test/test.csv")
+logger.info('bankcd:preprocess.py ends')
--- pipelines/bankcd/training.py
+++ pipelines/bankcd/training.py
@@ -10,11 +10,13 @@ from IPython.display import display
 from time import gmtime, strftime
 from sagemaker.predictor import csv_serializer
 import logging
+logger = logging.getLogger('log')
+logging.basicConfig(level=logging.INFO) # Set logging level and STDOUT handler
 
 # Define IAM Role
 role = get_execution_role()
 
-prefix = 'sagemaker/BankCd-xgboost-dm'
+prefix = 'xgb'
 my_region = boto3.session.Session().region_name
 bucket_name = 'tcb-bankcd'
 
@@ -24,27 +26,26 @@ bucket_name = 'tcb-bankcd'
 #     print('Success: Data loaded into dataframe.')
 # except Exception as e:
 #     print('Data load error: ',e)
-
-# this line automatically looks for the XGBoost image URI and builds an XGBoost container.
-xgboost_container = sagemaker.image_uris.retrieve("xgboost", my_region, "latest")
-
-print("Success - the MySageMakerInstance is in the " + my_region + " region. You will use the " + xgboost_container + " container for your SageMaker endpoint.")
-
-# Upload training data into S3 bucket
-# boto3.Session().resource('s3').Bucket(bucket_name).Object(os.path.join(prefix, 'train/train.csv')).upload_file('/opt/ml/processing/train/train.csv')
-s3_input_train = sagemaker.inputs.TrainingInput(s3_data='s3://{}/{}/train'.format(bucket_name, prefix), content_type='csv')
-
-
-sess = sagemaker.Session()
-xgb = sagemaker.estimator.Estimator(xgboost_container,role, instance_count=1, instance_type='ml.m4.xlarge',output_path='s3://{}/{}/output'.format(bucket_name, prefix),sagemaker_session=sess)
-xgb.set_hyperparameters(max_depth=5,eta=0.2,gamma=4,min_child_weight=6,subsample=0.8,silent=0,objective='binary:logistic',num_round=100)
-
-# Fit model on training data and deploy model
-xgb.fit({'train': s3_input_train})
-xgb_predictor = xgb.deploy(initial_instance_count=1,instance_type='ml.m4.xlarge')
-
+try:
+    # this line automatically looks for the XGBoost image URI and builds an XGBoost container.
+    xgboost_container = sagemaker.image_uris.retrieve("xgboost", my_region, "latest")
+    logger.info("Success - the MySageMakerInstance is in the " + my_region + " region. You will use the " + xgboost_container + " container for your SageMaker endpoint.")
+except Exception as e:
+    logger.error('Failure in creating xgboost container ', e)
+try:
+    train_input = sagemaker.inputs.TrainingInput(s3_data='s3://{}/{}/{}'.format(bucket_name, prefix,"train/train.csv"), content_type='csv')
+    validation_input = sagemaker.inputs.TrainingInput("s3://{}/{}/{}".format(bucket_name, prefix, "test/test.csv"), content_type="csv")
+    sess = sagemaker.Session()
+    xgb_model = sagemaker.estimator.Estimator(xgboost_container,role, instance_count=1, instance_type='ml.m4.xlarge',output_path='s3://{}/{}/output'.format(bucket_name, prefix),sagemaker_session=sess)
+    xgb_model.set_hyperparameters(max_depth=5,eta=0.2,gamma=4,min_child_weight=6,subsample=0.8,silent=0,objective='binary:logistic',num_round=100)
+    # Fit model on training data and deploy model
+    xgb_model.fit({'train': train_input, "validation": validation_input}, wait=True)
+    logger.info('TCB XGboost demo model training completed', xgb_model.model_data)
+except Exception as e:
+    logger.error('Failure in train model ', e)
+#xgb_predictor = xgb.deploy(initial_instance_count=1,instance_type='ml.m4.xlarge')
 # Save model as a pickle file to be used in evaluation.py
-filename = 'xgb_pred.pkl'
-pickle.dump(xgb_predictor, open(filename, 'wb'))
-logging.info('TCB XGboost demo model training completed')
+# filename = 'xgb_pred.pkl'
+# pickle.dump(xgb, open(filename, 'wb'))
+
 
